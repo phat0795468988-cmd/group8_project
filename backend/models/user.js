8d91f6e7841ed76b6228c178c5e89a0616a272e1
phat0795468988-cmd <phat0795468988@gmail.com>
2025-10-31 21:14:05 +0700

    fix user

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
-  },
-  { timestamps: true }
-);
-=======
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -71,7 +61,7 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
->>>>>>> 43478850ca17be44f0d22a2b5dbc4f44032cb474
+
 
 // Mã hoá mật khẩu trước khi lưu
 userSchema.pre("save", async function (next) {
